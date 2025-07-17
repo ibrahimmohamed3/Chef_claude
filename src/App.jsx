ced85d44172c690ebc94aefca5730e9dd621bf69
ibrahim <ibra39167271@gmail.com>
2025-07-17 14:10:09 +0300

    add the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header'
+import Main from './components/Main'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     <Header />
+    <Main />
     </>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import '../App.css'
+function Main(){
+    return(
+        <main>
+            <form className="add-ingredient-form">
+                <input type="text" placeholder="Ingredient" />
+                <button>Add Ingredient</button>
+            </form>
+        </main>
+    )
+}
+
+export default Main
