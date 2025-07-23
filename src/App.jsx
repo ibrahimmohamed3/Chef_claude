0d0c2e64d2a64bc2e47f02b7ff6710d0c170de9c
ibrahim <ibra39167271@gmail.com>
2025-07-23 22:00:13 +0300

    make changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import Header from './components/Header'
-import Main from './components/Main'
-import Count from './components/Count'
-import './App.css'
+// import Header from './components/Header'
+// import Main from './components/Main'
+import { useState } from "react";
+import "./App.css";
+import pads from "./components/pads";
 
-function App() {
- 
+function App(props) {
+const[color, setColor]=useState(pads)
+
+
+
+  mappedColors = pads.map(item=>(
+  <button key={pads.id}></button>
+))
 
   return (
     <>
-    <Header />
-    <Main />
-    <Count />
-  
-    
+      {/* <Header />
+    <Main /> */}
+
+      <main>
+         <div className="pad-container">{mappedColors}</div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
